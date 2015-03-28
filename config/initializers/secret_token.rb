597a294f3e0b6f2a5e5cb800b8d5fb9788364a1b
Marc Mentis <mmentis@optonline.net>
2015-03-28 11:19:41 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrashDeptestNoTables::Application.config.secret_key_base = 'cfec875e7fbb080062803c0ecc2bbd267792b60416fd9341ca4517adac4e5424561852a3a7fc88403ae4d336a8af93a709d85cceb9f46633121f4144720dfc56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrashDeptestNoTables::Application.config.session_store :cookie_store, key: '_trash_deptest_no_tables_session'
